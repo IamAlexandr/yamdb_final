aa30fa3a3b8644f4bdd7c31bb5f634adfc10e183
IamAlexandr <alex260791@yandex.ru>
2023-05-21 20:40:26 +0300

    v4

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -115,6 +115,8 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static/')),
                     ('redoc', '/app/templates/')]
+STATIC_ROOT = [(os.path.join(BASE_DIR, 'static')),
+               ('redoc', '/app/templates/')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
