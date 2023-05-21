4367eb029e13fd0a2f769ed86c9ec61d8bf6ff00
IamAlexandr <alex260791@yandex.ru>
2023-05-21 15:13:41 +0300

    v4

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,8 +11,3 @@ urlpatterns = [
         name='redoc'
     ),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
